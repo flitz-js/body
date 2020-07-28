426a9fe910932629b32615a53996d272dfd8d763
Marcel Kloubert <marcel.kloubert@gmx.net>
2020-07-28 22:35:07 +0200

    pull requests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # @flitz/body
 
-## 1.0.0
+## 1.0.1
 
 * first stable release
 
--- index.d.ts
+++ index.d.ts
@@ -21,7 +21,7 @@
 import flitz from 'flitz';
 
 declare module 'flitz' {
-  interface Request {
+  export interface Request {
     /**
      * The body, if parsed.
      */
